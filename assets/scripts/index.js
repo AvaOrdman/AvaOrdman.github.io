b3414958ae58b656e1b4b021933ccdf75a3b70a3
ericbowman <ericjbowman@gmail.com>
2019-10-24 22:44:26 -0400

    Comment out index.js to test css

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,62 +1,62 @@
-'use strict'
-
-const indexDisplay = require('./templates/index-gigs.handlebars')
-const events = require('./events.js')
-const api = require('./api.js')
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// use require without a reference to ensure a file is bundled
-// require('./example')
-
-$(() => {
-  $('#recordings-page').hide()
-  $('#calendar-page').hide()
-  $('#contact-page').hide()
-  $('#bio-page').hide()
-  $('#students-page').hide()
-  events.addHandlers()
-  const blurbs = function () {
-    setTimeout(function () {
-      $('#blurb3').removeClass('disappear')
-    }, 1000)
-    setTimeout(function () {
-      $('#spotify-player').removeClass('disappear')
-      $('#blurb2').removeClass('disappear')
-    }, 2000)
-    setTimeout(function () {
-      $('#blurb4').removeClass('disappear')
-    }, 2500)
-    setTimeout(function () {
-      $('#blurb5').removeClass('disappear')
-    }, 3000)
-  }
-  $(document).ready(function () {
-    blurbs()
-  })
-  // setTimeout(function () {
-  //   $('#spotify-player').removeClass('disappear')
-  //   $('#blurb2').removeClass('disappear')
-  // }, 1000)
-  // setTimeout(function () {
-  //   $('#blurb3').removeClass('disappear')
-  // }, 2000)
-  // setTimeout(function () {
-  //   $('#blurb4').removeClass('disappear')
-  // }, 2500)
-  const sortByDate = function (gigs) {
-    return gigs.sort(function compareNumbers (a, b) {
-      return (new Date(a.date).getTime()) - (new Date(b.date).getTime())
-    })
-  }
-
-  api.indexGigs()
-    .then((responseData) => $('.gigs-container').html(indexDisplay({ gigs: sortByDate(responseData.gigs) })))
-    // .then((responseData) => $('#signInSuccess').html(editGigs({ gigs: responseData.gigs.reverse() })))
-    .then(() => {
-      $('.calendar-loader').hide()
-      $('.calendar-load').hide()
-    })
-    .catch(console.log)
-})
+// 'use strict'
+//
+// const indexDisplay = require('./templates/index-gigs.handlebars')
+// const events = require('./events.js')
+// const api = require('./api.js')
+//
+// // use require with a reference to bundle the file and use it in this file
+// // const example = require('./example')
+//
+// // use require without a reference to ensure a file is bundled
+// // require('./example')
+//
+// $(() => {
+//   $('#recordings-page').hide()
+//   $('#calendar-page').hide()
+//   $('#contact-page').hide()
+//   $('#bio-page').hide()
+//   $('#students-page').hide()
+//   events.addHandlers()
+//   const blurbs = function () {
+//     setTimeout(function () {
+//       $('#blurb3').removeClass('disappear')
+//     }, 1000)
+//     setTimeout(function () {
+//       $('#spotify-player').removeClass('disappear')
+//       $('#blurb2').removeClass('disappear')
+//     }, 2000)
+//     setTimeout(function () {
+//       $('#blurb4').removeClass('disappear')
+//     }, 2500)
+//     setTimeout(function () {
+//       $('#blurb5').removeClass('disappear')
+//     }, 3000)
+//   }
+//   $(document).ready(function () {
+//     blurbs()
+//   })
+//   // setTimeout(function () {
+//   //   $('#spotify-player').removeClass('disappear')
+//   //   $('#blurb2').removeClass('disappear')
+//   // }, 1000)
+//   // setTimeout(function () {
+//   //   $('#blurb3').removeClass('disappear')
+//   // }, 2000)
+//   // setTimeout(function () {
+//   //   $('#blurb4').removeClass('disappear')
+//   // }, 2500)
+//   const sortByDate = function (gigs) {
+//     return gigs.sort(function compareNumbers (a, b) {
+//       return (new Date(a.date).getTime()) - (new Date(b.date).getTime())
+//     })
+//   }
+//
+//   api.indexGigs()
+//     .then((responseData) => $('.gigs-container').html(indexDisplay({ gigs: sortByDate(responseData.gigs) })))
+//     // .then((responseData) => $('#signInSuccess').html(editGigs({ gigs: responseData.gigs.reverse() })))
+//     .then(() => {
+//       $('.calendar-loader').hide()
+//       $('.calendar-load').hide()
+//     })
+//     .catch(console.log)
+// })
